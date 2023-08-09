f8a55886c8683dce01bc48fdd1fca96cd803e4da
Guillem Gari <ggari@robotnik.es>
2023-08-09 16:19:57 +0200

    Corrected python syntax

--- rb_theron_gazebo/launch/spawn.launch.py
+++ rb_theron_gazebo/launch/spawn.launch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Robotnik Automation S.L.L.
+# Copyright (c) 2023, Robotnik Automation S.L.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -23,62 +23,96 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os, launch, launch_ros
+import os
+import launch
+import launch_ros
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.event_handlers import OnProcessExit
 
 from robotnik_common.launch import add_launch_args
 
+
 def generate_launch_description():
 
-  ld = launch.LaunchDescription()
-  p = [
-    ('robot_id', 'Id of the robot', 'robot'),
-    ('robot_description_file', 'URDF file to load', 'default.urdf.xacro'),
-    ('pos_x', 'X position of the robot', '0.0'),
-    ('pos_y', 'Y position of the robot', '0.0'),
-    ('pos_z', 'Z position of the robot', '0.1')
-  ]
-  params = add_launch_args(ld, p)
+    ld = launch.LaunchDescription()
+    p = [
+        (
+            'robot_id',
+            'Id of the robot',
+            'robot'
+        ),
+        (
+            'robot_description_file',
+            'URDF file to load',
+            'default.urdf.xacro'
+        ),
+        (
+            'pos_x',
+            'X position of the robot',
+            '0.0'
+        ),
+        (
+            'pos_y',
+            'Y position of the robot',
+            '0.0'
+        ),
+        (
+            'pos_z',
+            'Z position of the robot',
+            '0.1'
+        )
+    ]
+    params = add_launch_args(ld, p)
 
-  # Node to spawn the robot in Gazebo
-  ld.add_action(launch.actions.IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(
-      os.path.join(get_package_share_directory('rb_theron_gazebo'), 'launch', 'description.launch.py')
-    ),
-    launch_arguments={
-      'use_sim_time': 'true',
-      'robot_id': params['robot_id'],
-      'robot_description_file': params['robot_description_file'],
-    }.items(),
-  ))
+    # Node to spawn the robot in Gazebo
+    ld.add_action(
+        launch.actions.IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('rb_theron_gazebo'),
+                    'launch',
+                    'description.launch.py'
+                )
+            ),
+            launch_arguments={
+                'use_sim_time': 'true',
+                'robot_id': params['robot_id'],
+                'robot_description_file': params['robot_description_file'],
+            }.items(),
+        )
+    )
 
-  ld.add_action(launch_ros.actions.Node(
-    namespace=params['robot_id'],
-    package='gazebo_ros',
-    executable='spawn_entity.py',
-    arguments=[
-      '-entity', params['robot_id'],
-      '-topic', 'robot_description',
-      '-x', params['pos_x'],
-      '-y', params['pos_y'],
-      '-z', params['pos_z'],
-    ],
-  ))
+    ld.add_action(
+        launch_ros.actions.Node(
+            namespace=params['robot_id'],
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', params['robot_id'],
+                '-topic', 'robot_description',
+                '-x', params['pos_x'],
+                '-y', params['pos_y'],
+                '-z', params['pos_z'],
+            ],
+        )
+    )
 
-  ld.add_action(launch_ros.actions.Node(
-    namespace=params['robot_id'],
-    package="controller_manager",
-    executable="spawner",
-    arguments=["robotnik_base_control"]
-  ))
+    ld.add_action(
+        launch_ros.actions.Node(
+            namespace=params['robot_id'],
+            package="controller_manager",
+            executable="spawner",
+            arguments=["robotnik_base_control"]
+        )
+    )
 
-  ld.add_action(launch_ros.actions.Node(
-    namespace=params['robot_id'],
-    package="controller_manager",
-    executable="spawner",
-    arguments=["joint_state_broadcaster"]
-  ))
+    ld.add_action(
+        launch_ros.actions.Node(
+            namespace=params['robot_id'],
+            package="controller_manager",
+            executable="spawner",
+            arguments=["joint_state_broadcaster"]
+        )
+    )
 
-  return ld
+    return ld
